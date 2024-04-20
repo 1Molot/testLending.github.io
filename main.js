74e58b069261e72f571147cc46bf1fb532746dcd
1Molot <sinkevich1990@gmail.com>
2024-04-20 19:18:40 +0300

    fix: css and adaptive 768px

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-const tabs = document.querySelectorAll('.tab');
-const tabContents = document.querySelectorAll('.tab-content');
+// const tabs = document.querySelectorAll('.tab');
+// const tabContents = document.querySelectorAll('.tab-content');
+//
+// tabs.forEach((tab, index) => {
+//     tab.addEventListener('click', () => {
+//         tabs.forEach(tab => tab.classList.remove('active'));
+//         tabContents.forEach(content => content.classList.remove('active'));
+//
+//         tab.classList.add('active');
+//         tabContents[index].classList.add('active');
+//     });
+// });
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(tab => tab.classList.remove('active'));
-        tabContents.forEach(content => content.classList.remove('active'));
 
-        tab.classList.add('active');
-        tabContents[index].classList.add('active');
-    });
+// active button
+const button = document.querySelector('.button__travels');
+const organizationButton = document.querySelector('.button__organization');
+const employeesButton = document.querySelector('.button__employees');
+
+button.addEventListener('click', function() {
+    button.classList.toggle('active');
+});
+
+organizationButton.addEventListener('click', function() {
+    organizationButton.classList.toggle('active');
+});
+
+employeesButton.addEventListener('click', function() {
+    employeesButton.classList.toggle('active');
 });
